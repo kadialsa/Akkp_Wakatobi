622a04e271283a04804429df4bf56388bf220c71
kadialsa <sukardinalsa23@gmail.com>
2026-04-14 13:55:25 +0800

    fix helper autoload

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,17 @@
 <?php
 
+
 use App\Http\Middleware\AuthAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\TrackVisitor;
 
-/** @var \Illuminate\Foundation\Application $app */
 
-$app = Application::configure(basePath: dirname(__DIR__))
+
+require_once base_path('app/Helpers/helpers.php');
+
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
@@ -24,9 +27,11 @@ $app = Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    });
-
-    $app->bind('path.public', function () {
-        return realpath(__DIR__ . '/../../public_html/profil');
-    });
-return $app->create();
+    })
+    ->withBindings([
+        // 🔥 INI PENGGANTI bind()
+        'path.public' => function () {
+            return base_path('../public_html/profil');
+        },
+    ])
+    ->create();
